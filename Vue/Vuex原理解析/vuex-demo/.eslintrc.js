31f13a3961fd7bdddb9dea4dffb429cf90c2dc03
丁星星 <1693992774@qq.com>
2020-07-19 23:04:28 +0800

    修改eslint配置+页面数据源

--- Vue/Vuex原理解析/vuex-demo/.eslintrc.js
+++ Vue/Vuex原理解析/vuex-demo/.eslintrc.js
@@ -10,8 +10,13 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
+  // rules: {
+  //   'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  //   'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  //   
+  // },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "space-before-function-paren": 0,
+    'no-irregular-whitespace': 'off'
   }
 }
--- Vue/Vuex原理解析/vuex-demo/src/App.vue
+++ Vue/Vuex原理解析/vuex-demo/src/App.vue
@@ -2,14 +2,25 @@
   <div id="app">
     <div>
       学习的年龄{{$store.state.age}}
-      <button>+1</button>
+      <br>
+      我的年龄 {{$store.getters.myAge}}
+      <button @click="syncChange()">点我更改</button>
+      <button @click="asyncChange()">异步更改</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  methods: {
+    syncChange() {
+      this.$store.commit('syncChange', 10)
+    },
+    asyncChange() {
+      this.$store.dispatch('asyncChange', 5)
+    }
+  }
 }
 </script>
 
