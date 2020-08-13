b3859776dc846190caab97a7cecb9f2cdca2c966
dxiaoxing <1693992774@qq.com>
2020-08-13 19:25:48 +0800

    新增echop爬取音乐开头

---
+++ 爬虫/Reptiles爬虫/echo/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: dxiaoxing
+ * @Date: 2020-08-13 19:15:16
+ * @LastEditors: dxiaoxing
+ * @LastEditTime: 2020-08-13 19:17:07
+ */
+
+
+
+// 目标：下载音乐
+// 1获取音乐相关的信息，通过音乐相关的信息获取mp3地址
+// 2如何获取大量的音乐信息，通过获取音乐列表
+// 3通过音乐的分类页，获取音乐列表
+
+
+
