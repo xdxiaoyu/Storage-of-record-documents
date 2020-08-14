c2e734e228c7e2b8364c221b7b22bd94a7a2e5f3
dxiaoxing <1693992774@qq.com>
2020-08-14 18:43:53 +0800

    爬取echo下载音乐

--- 爬虫/Reptiles爬虫/echo/index.js
+++ 爬虫/Reptiles爬虫/echo/index.js
@@ -4,8 +4,11 @@
  * @Author: dxiaoxing
  * @Date: 2020-08-13 19:15:16
  * @LastEditors: dxiaoxing
- * @LastEditTime: 2020-08-13 19:17:07
+ * @LastEditTime: 2020-08-14 18:38:47
  */
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
 
 
 
@@ -15,4 +18,36 @@
 // 3通过音乐的分类页，获取音乐列表
 
 
+// 获取音乐
+async function getPage(num) {
+  let httpUrl = "http://www.app-echo.com/api/recommend/sound-day?page=" + num
+  let res = await axios.get(httpUrl)
 
+  res.data.list.forEach((item, i) => {
+    let title = item.sound.name
+    let mp3Url = item.sound.source
+    let filename = path.parse(mp3Url).name
+
+    let content = `${title},${mp3Url},${filename}\n`
+    fs.writeFile('music.txt', content, {flag:'a'},() => {
+      console.log('写入完成：'+ title);
+    })
+    // console.log(title);
+    // console.log(mp3Url);
+    download(mp3Url,filename)
+  })
+}
+
+// 下载音乐
+async function download(mp3Url, filename) {
+  let res = await axios.get(mp3Url, { responseType: "stream" })
+  let ws = fs.createWriteStream('./mp3/' + filename + '.mp3')
+  res.data.pipe(ws)
+  // res.data.on('close',() => {
+  //   ws.close()
+  // })
+}
+
+for(i=1;i<=5; i++) {
+  getPage(i)
+}
